e23de9a4f78c167546041cd6799ffc5ce456ac2f
Paulina Murzanska <p.murzanska@gmail.com>
2021-05-03 07:43:15 -0700

    Education site added.

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import '../../scss/pageContainer.scss';
-import {ROUTE_ABOUT} from "Constants/Routes";
-import {Link} from "react-router-dom";
-import {ImCross} from "react-icons/im";
+import SchoolBlock from "components/Education/SchoolBlock";
+import uek from '../../images/uek.jpg';
+import codeb from "../../images/yellow bus.png";
+import lo from "../../images/lo.jpeg";
 
 const Education = () => {
     return (
@@ -10,8 +11,44 @@ const Education = () => {
             <div className="pages">
                 <div className="pages-container">
                     <div className="container-text">
-                        <div className="inner-container">
-                            <p>education</p>
+                        <div className="inner-container education">
+                            <div className="education-site-wrapper">
+                                <SchoolBlock
+                                    imageUlr={codeb}
+                                    schoolName="CodeBrainers"
+                                    reversed={true}
+                                    schoolNameText="CodeBrainers bootcamp"
+                                    degree1="Full stack developer | 2020 - 2021"
+                                    subject1=''
+                                    degree2=""
+                                    subject2=''
+                                    additionalClass="codebr"
+                                />
+                                <SchoolBlock
+                                    imageUlr={uek}
+                                    schoolName="Cracow University of Economics"
+                                    reversed={false}
+                                    schoolNameText="Cracow University of Economics"
+                                    schoolNameText2="Cracow University of Economics"
+                                    degree1="Master of Economics | 2010 - 2013"
+                                    subject1='Specialization: HR Management and Consulting'
+                                    degree2="Bachelor of Economics | 2006 - 2010"
+                                    subject2='Specialization: International Economic Relations'
+                                    additionalClass=""
+                                />
+                                   <SchoolBlock
+                                    imageUlr={lo}
+                                    schoolName="I LO im. T.Kościuszki in Myślenice"
+                                    reversed={true}
+                                    schoolNameText="I LO im. T.Kościuszki in Myślenice"
+                                    schoolNameText2=""
+                                    degree1="Math - Phys profile class | 2003 - 2006"
+                                    subject1=''
+                                    degree2=""
+                                    subject2=''
+                                    additionalClass=""
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
---
+++ src/components/Education/SchoolBlock.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './SchoolBlock.scss';
+
+const SchoolBlock = ({imageUlr,schoolNameText, additionalClass,schoolNameText2,schoolName, reversed, degree1, subject1, degree2, subject2}) => {
+    return (
+        <React.Fragment>
+
+            <div
+                className={`school-block ${reversed ? "reversed" : ""}`}
+            >
+                <div className={`image-block ${additionalClass}`}
+                     style={{backgroundImage: `url(${imageUlr})`}}
+                >
+                    <p>{schoolName}</p>
+                </div>
+                <div className="details-block">
+                    <div className="edu-block">
+                        <h2>{schoolNameText}</h2>
+                        <h3>{degree1}</h3>
+                        <h4>{subject1}</h4>
+                    </div>
+                    <div className="edu-block">
+                        <h2>{schoolNameText2}</h2>
+                        <h3>{degree2}</h3>
+                        <h4>{subject2}</h4>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </React.Fragment>
+
+    )
+}
+export default SchoolBlock;
